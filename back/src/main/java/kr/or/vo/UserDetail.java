aa90239dac65d5f073590de9e998f8613bf6af08
park71403 <park71403@gmail.com>
2023-01-27 17:48:19 +0900

    jwt 시도

---
+++ back/src/main/java/kr/or/vo/UserDetail.java
@@ -0,0 +1,15 @@
+package kr.or.vo;
+
+import lombok.Data;
+
+@Data
+public class UserDetail {
+
+	private int u_idx;
+	private String company;
+	private String bio;
+	private String email;
+	private String location;
+	private String blog;
+	
+}
