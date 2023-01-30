f90bde6653fc9a3606a9dace4787755e151af1b1
DESKTOP-EDMHTIC\edhd3 <edhd3692@naver.com>
2023-01-30 19:18:26 +0900

    230130 오후 yunseop

--- front/src/views/kanban/Kanban.js
+++ front/src/views/kanban/Kanban.js
@@ -1,4 +1,7 @@
-import { CCard, CCardBody, CCardFooter, CCol, CRow } from "@coreui/react"
+import { CCard, CCardBody, CCardFooter, CCol, CDropdown, CDropdownItem, CDropdownMenu, CDropdownToggle, CRow } from "@coreui/react"
+import CIcon from '@coreui/icons-react'
+
+import * as icon from '@coreui/icons';
 import WidgetsDropdown from "../widgets/WidgetsDropdown"
 
 const Kanban = () => {
@@ -6,11 +9,85 @@ const Kanban = () => {
     <>
       <CCard className="mb-4">
         <CCardBody>
-        
-        </CCardBody>
-        <CCardFooter>
+          <CRow>
+            <CCard className="col-md-3">
+              <CRow>
+                <CCol xs="auto" className="me-auto">
+                    To-Do
+                </CCol>
+                <CCol xs="auto"> <CDropdown alignment="end">
+                <CDropdownToggle color="transparent" caret={false} className="p-0">
+                  <CIcon icon={icon.cilOptions} />
+                </CDropdownToggle>
+                <CDropdownMenu>
+                  <CDropdownItem>Action</CDropdownItem>
+                  <CDropdownItem>Another action</CDropdownItem>
+                  <CDropdownItem>Something else here...</CDropdownItem>
+                  <CDropdownItem disabled>Disabled action</CDropdownItem>
+                </CDropdownMenu>
+              </CDropdown></CCol>
+                
+              </CRow>
 
-        </CCardFooter>
+
+            </CCard>
+
+
+            <CCard className="col-md-3">
+              <CRow>
+                <CCol xs="auto" className="me-auto">
+                    In Progress
+                </CCol>
+                <CCol xs="auto"> <CDropdown alignment="end">
+                <CDropdownToggle color="transparent" caret={false} className="p-0">
+                  <CIcon icon={icon.cilOptions} />
+                </CDropdownToggle>
+                <CDropdownMenu>
+                  <CDropdownItem>Action</CDropdownItem>
+                  <CDropdownItem>Another action</CDropdownItem>
+                  <CDropdownItem>Something else here...</CDropdownItem>
+                  <CDropdownItem disabled>Disabled action</CDropdownItem>
+                </CDropdownMenu>
+              </CDropdown></CCol>
+                
+              </CRow>
+
+
+            </CCard>
+
+
+            <CCard className="col-md-3">
+              <CRow>
+                <CCol xs="auto" className="me-auto">
+                    Done
+                </CCol>
+                <CCol xs="auto"> <CDropdown alignment="end">
+                <CDropdownToggle color="transparent" caret={false} className="p-0">
+                  <CIcon icon={icon.cilOptions} />
+                </CDropdownToggle>
+                <CDropdownMenu>
+                  <CDropdownItem>Action</CDropdownItem>
+                  <CDropdownItem>Another action</CDropdownItem>
+                  <CDropdownItem>Something else here...</CDropdownItem>
+                  <CDropdownItem disabled>Disabled action</CDropdownItem>
+                </CDropdownMenu>
+              </CDropdown></CCol>
+                
+              </CRow>
+
+
+            </CCard>
+            <CCard className="col-md-3">
+              <CIcon
+                icon={icon.cilPlus}
+                onClick={() => {
+                  alert('플러스 클릭')
+                }}
+              />
+            </CCard>
+          </CRow>
+        </CCardBody>
+        <CCardFooter></CCardFooter>
       </CCard>
     </>
   )
