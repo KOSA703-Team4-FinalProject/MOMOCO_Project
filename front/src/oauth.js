8f9629857f1e6a2b05c7b767fe9ad7786397ba7b
park71403 <park71403@gmail.com>
2023-02-02 15:03:30 +0900

    로그인 시도 중....

---
+++ front/src/oauth.js
@@ -0,0 +1,8 @@
+
+export const CLIENT_ID = "31c68970a72ad9048531";
+export const CLIENT_SECRETS = "bef68d247681c7b728bdcf444b7dd93494dbe40a";
+export const CALLBACK_URL = "http://localhost:3000/oauth/callback";
+
+export const GITHUB_AUTH_CODE_SERVER = "/login/oauth/authorize";
+export const GITHUB_AUTH_TOKEN_SERVER = "/login/oauth/access_token";
+export const GITHUB_API_SERVER = "/user";
