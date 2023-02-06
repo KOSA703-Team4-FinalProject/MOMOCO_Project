457160fa5ea4baadf47a994ba881691bcfcc3c06
park71403 <park71403@gmail.com>
2023-02-06 20:38:54 +0900

    종민쓰 mybatis 문제 해결 + 인터셉터 에러 해결

--- back/src/main/java/kr/or/utils/LoginInterceptor.java
+++ back/src/main/java/kr/or/utils/LoginInterceptor.java
@@ -7,24 +7,29 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class LoginInterceptor implements HandlerInterceptor {
-	
-	
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		System.out.println("preHandler 실행해서 로그인 유무 확인할 거임");
 		boolean result = false;
-		
+
 		Cookie[] cookies = request.getCookies();
-		
-		for(Cookie c : cookies) {
-			System.out.println(c.getName());
-			System.out.println(c.getValue());
-			if(c.getName() == "githubToken") {
+
+		for (Cookie c : cookies) {
+			if (c.getName().equals("githubToken")) {
 				result = true;
 			}
 		}
+
+		System.out.println(request.getRequestURI());
 		
+		if(request.getRequestURI().equals("/controller/backlogin/login")) {
+			result = true;
+		}
+		
+		System.out.println("result : " + result);
+
 		return result;
 	}
 
