a2cfce8da28dbeb7c7dfb255884033c31bce6709
park71403 <park71403@gmail.com>
2023-02-06 10:52:48 +0900

    front 쪽 access token 작업 끝
    access_token 받아서 쿠키에 저장 완료

--- front/src/views/login/Profile.js
+++ front/src/views/login/Profile.js
@@ -1,34 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useLocation } from 'react-router-dom'
+import { Cookies } from 'react-cookie';
+
 import { GITHUB_API_SERVER } from '../../oauth'
+import { getCookieToken } from './Cookie'
 
 const Profile = () => {
-  const location = useLocation();
-  const [userName, setUserName] = useState();
-
-  console.log(location);
+  const [userName, setUserName] = useState()
 
   useEffect(() => {
     const fetchGithubUser = () => {
-      const accessToken = location.state;
+      const accessToken = getCookieToken(); //cookie에서 토큰 불러오기
 
       return fetch(GITHUB_API_SERVER, {
-        method: "GET",
+        method: 'GET',
         headers: {
           Authorization: `Bearer ${accessToken}`,
-          Accept: "application/json",
+          Accept: 'application/json',
         },
-      });
-    };
+      })
+    }
 
     fetchGithubUser()
       .then((response) => response.json())
       .then(({ login }) => setUserName(login))
-      .catch((err) => console.log(err));
-  });
+      .catch((err) => console.log(err))
+  })
 
-  return <div>로그인 된 사용자 : {userName ?? "로딩중.."}</div>;
-};
+  return <div>로그인 된 사용자 : {userName ?? '로딩중..'}</div>
+}
 
 export default Profile
