1a1823fd4ede30770226a4b8512d671ff15f8d5a
park71403 <park71403@gmail.com>
2023-02-06 09:19:53 +0900

    로그인 시도 중

--- front/src/views/login/Profile.js
+++ front/src/views/login/Profile.js
@@ -4,31 +4,31 @@ import { useLocation } from 'react-router-dom'
 import { GITHUB_API_SERVER } from '../../oauth'
 
 const Profile = () => {
-  const location = useLocation()
-  const dispatch = useDispatch()
+  const location = useLocation();
+  const [userName, setUserName] = useState();
 
-  let gitToken = useSelector((state) => state.gitToken)
+  console.log(location);
 
   useEffect(() => {
     const fetchGithubUser = () => {
-      const accessToken = location.state
+      const accessToken = location.state;
 
       return fetch(GITHUB_API_SERVER, {
-        method: 'GET',
+        method: "GET",
         headers: {
-          Authorization: `Bearer ${gitToken}`,
-          Accept: 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+          Accept: "application/json",
         },
-      })
-    }
+      });
+    };
 
     fetchGithubUser()
       .then((response) => response.json())
-      .then(({ login }) => dispatch(  ) )
-      .catch((err) => console.log(err))
-  })
+      .then(({ login }) => setUserName(login))
+      .catch((err) => console.log(err));
+  });
 
-  return <div>로그인 된 사용자 : {userName ?? '로딩중..'}</div>
-}
+  return <div>로그인 된 사용자 : {userName ?? "로딩중.."}</div>;
+};
 
 export default Profile
