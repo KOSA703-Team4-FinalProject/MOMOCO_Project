ec286932ffb9a4159b238265a7cc8f59b1b2f4c7
(JM KIM) <hydrogen93@naver.com>
2023-02-07 21:02:51 +0900

    워크스페이스 생성시 프로시저 트랜잭셔날 처리

--- back/src/main/java/kr/or/dao/WorkSpaceDao.java
+++ back/src/main/java/kr/or/dao/WorkSpaceDao.java
@@ -12,4 +12,6 @@ public interface WorkSpaceDao {
    //워크스페이스 생성시 도메인 중복 체크
    public int isDomain(String url) throws ClassNotFoundException, SQLException;
    
+   //워크스페이스 생성시 테이블 create 프로시저 호출
+   public void createTable(String url) throws ClassNotFoundException, SQLException;
 }
