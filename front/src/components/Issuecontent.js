2f4697e4be3a278a223062716b5039e98c009315
hyejin0810 <heajin0810@naver.com>
2023-02-09 17:15:27 +0900

    이슈링크한번만클릭됨

--- front/src/components/Issuecontent.js
+++ front/src/components/Issuecontent.js
@@ -1,6 +1,6 @@
 import { CButton, CCard, CCardBody, CCardHeader, CCardText, CCardTitle } from '@coreui/react'
 import { useDispatch, useSelector } from 'react-redux'
-import { updateissueNumber } from 'src/store'
+import { updateIssueModal, updateissueNumber } from 'src/store'
 
 const box = {
   width: '600px',
@@ -9,9 +9,13 @@ const box = {
 
 const Issuecontent = (props) => {
   const dispatch = useDispatch()
-
+  const issueModal = useSelector((state) => state.issueModal)
+  const issueNumber = useSelector((state) => state.issueNumber)
   const makeLink = (e) => {
     dispatch(updateissueNumber(e.target.value))
+    if (issueModal == true) {
+      return dispatch(updateIssueModal(false))
+    }
   }
 
   let issue = props.issuelist
