7dce36dc0a714707a8477ec0b340f59fdcc7e3d2
park71403 <park71403@gmail.com>
2023-02-13 17:11:10 +0900

    로그아웃 기능 완성

--- front/src/components/header/AppHeaderDropdown.js
+++ front/src/components/header/AppHeaderDropdown.js
@@ -23,8 +23,17 @@ import {
 import CIcon from '@coreui/icons-react'
 
 import avatar8 from './../../assets/images/avatars/8.jpg'
+import { useNavigate } from 'react-router-dom'
 
 const AppHeaderDropdown = () => {
+
+  const navigate = useNavigate();
+
+  const logout = () => {
+    window.localStorage.clear();
+    navigate('/');
+  }
+
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0" caret={false}>
@@ -84,9 +93,9 @@ const AppHeaderDropdown = () => {
           </CBadge>
         </CDropdownItem>
         <CDropdownDivider />
-        <CDropdownItem href="#">
+        <CDropdownItem onClick={logout}>
           <CIcon icon={cilLockLocked} className="me-2" />
-          Lock Account
+          로그아웃
         </CDropdownItem>
       </CDropdownMenu>
     </CDropdown>
