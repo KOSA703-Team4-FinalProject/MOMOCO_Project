ac9c0dea0295a651ae1560bf4bb0ae6059ded587
(JM KIM) <hydrogen93@naver.com>
2023-02-24 20:21:46 +0900

    멤버 초대

--- front/src/components/AppSidebar.js
+++ front/src/components/AppSidebar.js
@@ -1,13 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
-import { CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
+import {
+  CButton,
+  CCard,
+  CFormInput,
+  CModal,
+  CModalBody,
+  CModalHeader,
+  CSidebar,
+  CSidebarBrand,
+  CSidebarNav,
+} from '@coreui/react'
 
 import { AppSidebarNav } from './AppSidebarNav'
 import whitemomoco from '../assets/images/whitemomoco.png'
-import { logoNegative } from 'src/assets/brand/logo-negative'
-import { sygnet } from 'src/assets/brand/sygnet'
+import axios from 'axios'
 
 import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
@@ -30,8 +38,8 @@ const AppSidebar = () => {
       }}
     >
       <CSidebarBrand className="d-none d-md-flex my-1 py-2" to="/">
-        <img src={whitemomoco} width="55px" className='me-3' />
-        <h1 className='mt-1'>momoco</h1>
+        <img src={whitemomoco} width="55px" className="me-3" />
+        <h1 className="mt-1">momoco</h1>
       </CSidebarBrand>
       <CSidebarNav>
         <SimpleBar>
