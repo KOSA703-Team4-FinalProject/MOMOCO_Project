e30f5a89c3efbab5a0b019082fc279b6a7263c20
park71403 <park71403@gmail.com>
2023-02-24 23:56:27 +0900

    대시보드 백단 작업 중

--- back/src/main/java/kr/or/service/BoardSerivce.java
+++ back/src/main/java/kr/or/service/BoardSerivce.java
@@ -16,19 +16,20 @@ import kr.or.vo.CommonBoard;
 @Service
 public class BoardSerivce {
 	private SqlSession sqlsession;
+
 	@Autowired
 	public void setSqlsession(SqlSession sqlsession) {
-		this.sqlsession =sqlsession;
-		
+		this.sqlsession = sqlsession;
+
 	}
-	
-	//알람에서 사용, 해당 알람이 발생할 게시글 찾기
+
+	// 알람에서 사용, 해당 알람이 발생할 게시글 찾기
 	public Board getBoardByTitle(Board board) {
-		
+
 		Board reboard = new Board();
-		
+
 		try {
-			
+
 			BoardDao boarddao = sqlsession.getMapper(BoardDao.class);
 			reboard = boarddao.getBoardByTitle(board);
 			System.out.println("reboardgetbytitle : " + reboard);
@@ -37,14 +38,14 @@ public class BoardSerivce {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return reboard;
 	}
-	
-	//모든보드검색
-	public List<Board>allBoardList(Board url){
+
+	// 모든보드검색
+	public List<Board> allBoardList(Board url) {
 		List<Board> board = new ArrayList<Board>();
-		
+
 		try {
 			BoardDao boarddao = sqlsession.getMapper(BoardDao.class);
 			board = boarddao.allBoardList(url);
@@ -55,50 +56,64 @@ public class BoardSerivce {
 		}
 		return board;
 	}
-	
-	//보드번호 검색
-	public List<Board>searchboardnumber(Board search){
+
+	// 보드번호 검색
+	public List<Board> searchboardnumber(Board search) {
 		List<Board> board = new ArrayList<Board>();
 		try {
 			BoardDao boarddao = sqlsession.getMapper(BoardDao.class);
 			board = boarddao.searchboardnumber(search);
-		}catch (ClassNotFoundException e) {
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-			}catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
-			}
-	
+		}
+
+		return board;
+	}
+
+	// 검색
+	public List<Board> boardSearch(Board string) {
+		List<Board> board = new ArrayList<Board>();
+		try {
+			BoardDao boarddao = sqlsession.getMapper(BoardDao.class);
+			board = boarddao.boardSearch(string);
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return board;
+	}
+
+	// 자신이 읽지 않은글
+	public List<Board> notread(Board url) {
+		List<Board> board = new ArrayList<Board>();
+		try {
+			BoardDao boarddao = sqlsession.getMapper(BoardDao.class);
+			board = boarddao.notread(url);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		return board;
 	}
 	
-	//검색
-		public List<Board>boardSearch(Board string){
-			List<Board> board = new ArrayList<Board>();
-			try {
-				BoardDao boarddao = sqlsession.getMapper(BoardDao.class);
-				board =boarddao.boardSearch(string);
-				
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-				}catch (SQLException e) {
-				e.printStackTrace();
-				}
-			return board;
-		}
-
+	//최신글 4개 불러오기
+	public List<Board> getNewBoardList(String url) {
+		List<Board> boardlist = new ArrayList<Board>();
 		
-		//자신이 읽지 않은글 
-		
-		public List<Board>notread(Board url){
-			List<Board> board = new ArrayList<Board>();
-			try {
-				BoardDao boarddao = sqlsession.getMapper(BoardDao.class);
-				board = boarddao.notread(url);
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			return board;
+		try {
+			BoardDao boarddao = sqlsession.getMapper(BoardDao.class);
+			boardlist = boarddao.getNewBoardList(url);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+		
+		return boardlist;
+	}
 }
